80513ba24aa101fa5927b40050d33e2065d0b6ac
l11EG4 <zakariaberahmi@gmail.com>
2023-05-16 16:06:10 -0700

    add strcat

---
+++ strcat.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+* _strcat - concatenates 2 strings
+* @dstn: the destination of the buffer
+* @src: the source of the buffer
+* Made by Laila and Mega
+* Retrun: the pointer to the destionation of the buffer
+*/
+
+char *_strcat(char *dstn, char *scr)
+{
+	char *ret = dest;
+
+	while (*dstn)
+		dstn++;
+	while (*src)
+		*dstn++ = *src++;
+	*dstn = *src;
+	return (ret);
+}
