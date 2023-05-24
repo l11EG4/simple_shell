b638dc9b7e456a6072287b04907489b63acd3d8e
laila tabourit <lailatabourit@gmail.com>
2023-05-24 07:49:01 -0700

    ALX_tasks

--- splited_tokens.c
+++ splited_tokens.c
@@ -9,18 +9,26 @@
 */
 char **split_input(char *line)
 {
-	const char *delimiters = " \n", *tokens;
-	int number_tokens = 0, i;
+	const char *delimiters = " \n";
+	const char *tokens;
+	int number_tokens = 0, i, j;
 	char *line_copy;
 	char **argv;
 
 	line_copy = _strdup(line);
+	if (line_copy == NULL)
+	{
+		perror("_Strdup failed");
+		return (NULL);
+	}
 
 	/*----------Split the input string to an array-----*/
 	tokens = _strtok(line_copy, delimiters);
 	if (tokens == NULL)
+	{
+		free(line_copy);
 		return (NULL);
-
+	}
 	while (tokens != NULL)
 	{
 		number_tokens++;
@@ -29,14 +37,28 @@ char **split_input(char *line)
 	number_tokens++;
 	/*--Allocate space & stor each token in an array of strings(argv)--*/
 	argv = malloc(sizeof(char *) * number_tokens);
+	if (argv == NULL)
+	{
+		perror("malloc failed");
+		free(line_copy);
+		return (NULL);
+	}
 	tokens = _strtok(line, delimiters);
 	for (i = 0; tokens != NULL; i++)
 	{
 		argv[i] = _strdup(tokens);
+		if (argv[i] == NULL)
+		{
+			perror("strdup failed");
+			free(line_copy);
+			for (j = 0; j < i; j++)
+				free(argv[j]);
+			free(argv);
+			return (NULL);
+		}
 		tokens = _strtok(NULL, delimiters);
 	}
 	argv[i] = NULL;
 	free(line_copy);
-
 	return (argv);
 }
