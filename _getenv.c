ce555222b73127beb0e98649aeddcc469bb2571e
l11EG4 <zakariaberahmi@gmail.com>
2023-05-24 16:50:47 -0700

    add files

--- _getenv.c
+++ _getenv.c
@@ -1,6 +1,38 @@
 #include "shell"
 
 /**
-* _getenv - char function
+* get_env - char function
 * @nam: prm
-*
+* Made by Laila and Mega
+* Return: NULL
+*/
+
+char *get_env(const char *nam)
+{
+	int a, result;
+
+	for (a = 0; environ[a]; a++)
+	{
+		result = _PATHcmp(nam, environ[a]);
+		if (result == 0)
+		{
+			return (environ[a]);
+		}
+	}
+	return (NULL);
+}
+
+/**
+* _env - print the environ
+* Made by Laila and Mega
+* Return: zero
+*/
+
+int _env(void)
+{
+	int a;
+
+	for (a = 0; environ[a]; a++)
+		_puts(environ[a]);
+	return (0);
+}
