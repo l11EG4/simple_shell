42289a575e0a51a6640fc24a97395f17d6164c89
laila tabourit <lailatabourit@gmail.com>
2023-05-26 16:43:55 -0700

    ALX_tasks

--- getline.c
+++ getline.c
@@ -124,11 +124,12 @@ ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
 	if (input == -1)
 	{
 		free(buffer);
-			return (-1);
+		return (-1);
 	}
 
 	input = handle_spaces(buffer, input);
 	bring_line(lineptr, n, buffer, input);
 	retval = input;
+	input = 0;
 	return (retval);
 }
